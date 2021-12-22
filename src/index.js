aa068a1a6fe1b5889996229d86080dbedd720c18
Tsapov-Maxim <tsapov552@gmail.com>
2021-12-22 18:24:42 +0700

    Init

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Store from './store.js';
-import App from './app.js';
+import {BrowserRouter} from "react-router-dom";
+import Store from './store';
+import App from './app';
+import StoreProvider from "./store/provider";
+import * as modules from './store/exports.js';
 
-const root = document.getElementById('app');
+const root = document.getElementById("app");
 
 // Состояние приложения
-const store = new Store({
-  items: [
-    { code: 1, title: 'Название элемента', selectedCount: 0 },
-    { code: 2, title: 'Некий объект', selectedCount: 0 },
-    { code: 3, title: 'Заголовок', selectedCount: 0 },
-    { code: 4, title: 'Короткое название', selectedCount: 0 },
-    { code: 5, title: 'Запись', selectedCount: 0 },
-    { code: 6, title: 'Пример названия', selectedCount: 0 },
-    { code: 7, title: 'Седьмой', selectedCount: 0 },
-  ],
-});
+const store = new Store(modules);
 
 // Сообщаем реакту что и куда рендерить.
-store.subscribe(() => {
-  ReactDOM.render(<App store={store} />, root);
-});
-
-// Сообщаем реакту что и куда рендерить.
-ReactDOM.render(<App store={store} />, root);
+ReactDOM.render(
+  <StoreProvider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </StoreProvider>,
+  root
+);
